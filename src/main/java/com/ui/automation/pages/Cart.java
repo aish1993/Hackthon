f68dd0f0334fdd5037cb0a30a6c46d47bfeb77c2
aish1993 <62331253+aish1993@users.noreply.github.com>
2020-03-19 13:14:32 +0530

    Update Cart.java

--- src/main/java/com/ui/automation/pages/Cart.java
+++ src/main/java/com/ui/automation/pages/Cart.java
@@ -1,9 +1,9 @@
 package com.ui.automation.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Cart {
-
-	public Cart() {
-		// TODO Auto-generated constructor stub
-	}
-
+@FindBy(css = "span[class='B18_42'][class='cart-card-content-info-final-amount-rupees-icon']")
+public WebElement price;
 }
