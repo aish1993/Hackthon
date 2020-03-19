ffb55f00849decde3e72c59c85c05acf69eba038
aish1993 <62331253+aish1993@users.noreply.github.com>
2020-03-19 12:55:15 +0530

    Update HomePage.java

--- src/main/java/com/ui/automation/pages/HomePage.java
+++ src/main/java/com/ui/automation/pages/HomePage.java
@@ -1,9 +1,26 @@
 package com.ui.automation.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
 
-	public HomePage() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
+	@FindBy(xpath="//a[text()='All Categories']")
+	public WebElement allCatagory;
+	@FindBy(xpath="//a[text()='OFFERS']")
+	public WebElement offers;
+	@FindBy(xpath="//a[text()=' Health & Safety']")
+	public WebElement HealthAndSafety;
+ public void click_on_homepage(){
+	 allCatagory.click();
+System.out.println("Successfully clicked on All Catagory button");
+	 }
+ public void click_on_offers(){
+	 offers.click();
+	 System.out.println("Successfully clicked on Offers button");
+	 }
+ public void click_on_HealthAndSafety()
+ {HealthAndSafety.click();}
+ 
+ }
+ 
